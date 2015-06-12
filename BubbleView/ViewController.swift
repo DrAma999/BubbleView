10dc95ea96d2fb400edc8816e84af7f9ed31df9e
DrAma78 <andrea.finollo@faswebnet.it>
2015-06-12 10:28:02 +0200

    Initial Commit

---
+++ BubbleView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BubbleView
+//
+//  Created by Andrea Finollo on 12/06/15.
+//  Copyright (c) 2015 CloudInTouch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
